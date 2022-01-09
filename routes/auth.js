1a248f877e37bdb51c695cfb0a0e887d58b04ccd
Khailas R <khailas303@gmail.com>
2022-01-09 17:35:51 +0530

    login initialized

--- routes/auth.js
+++ routes/auth.js
@@ -5,20 +5,23 @@ const User = require('../model/user');
 const { registerValidation, loginValidation } = require('../validation');
 
 
+const options = {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true // remove unknown props
+};
+
+
 router.post('/register', async (req, res) => {
-    const options = {
-        abortEarly: false, // include all errors
-        allowUnknown: true, // ignore unknown props
-        stripUnknown: true // remove unknown props
-    };
-    
+
     // data validation
     const { error, value } = registerValidation(req.body, options);
 
     if (error) {
         return res.status(400)
             .send(error.details[0].message);
-    } else {
+    }
+    else {
         // on success replace req.body with validated value and trigger next middleware function
         req.body = value;
     }
@@ -27,11 +30,12 @@ router.post('/register', async (req, res) => {
     const saltRounds = 10;
     const hashPassword = bcrypt.hashSync(req.body.password, saltRounds);
 
-    
+
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
+
         password: hashPassword,
         repeatPassword: hashPassword,
         date: req.body.date,
@@ -39,26 +43,50 @@ router.post('/register', async (req, res) => {
 
     // checks user exists in the db already
     const emailExist = await User.findOne({ email: req.body.email });
-
+    
     if (emailExist) {
         return res.status(400)
             .send('Email already Exists');
     }
 
-
     try {
-        const savedUser = await user.save();
-        res.send(savedUser);
+        // const savedUser = await user.save();
+        // res.send(savedUser);
+        await user.save();
+        res.send({
+            user: user.firstName + ' ' + user.lastName,
+            email: user.email,
+        });
         console.log('User saved');
     }
     catch (error) {
         res.status(400).send(error);
-        console.log('user errror');
+        console.log('user error');
     }
 });
 
 
-router.post('/login');
+router.post('/login', async (req, res) => {
+
+    const { errror, value } = loginValidation(req.body, options);
+
+    if (errror) {
+        return res.status(400)
+            .send(errror.details[0].message);
+    }
+    else {
+        req.body = value;
+    }
+
+    const noEmailMatch = await User.findOne({ email: req.body.email });
+
+    if (!noEmailMatch) {  // if no matching email
+        return res.status(400)
+            .send('Email does not exist');
+    }
+
+    const validPswd = bcrypt.compare()
+});
 
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -3,37 +3,41 @@ const Joi = require('joi');
 const registerValidation = ((user) => {
 
     const JoiSchema = Joi.object({
-    
+
         firstName: Joi.string()
             .min(3)
             .max(100)
             .required(),
-    
+
         lastName: Joi.string()
             .min(3)
             .max(100)
             .required(),
-    
+
         email: Joi.string()
-            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+            .required(),
+
         password: Joi.string()
             .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
             .min(6)
             .max(100)
             .required(),
-    
-        repeatPassword: Joi.ref('password'),
-    
+
+        repeatPassword: Joi.string().
+            equal(Joi.ref('password')).
+            messages({ 'any.only': 'password does not match' })
+            .required(),
+
         accessToken: [
             Joi.string(),
             Joi.number()
         ],
-    
+
     })
         .xor('password', 'accessToken')
         .with('password', 'repeatPassword');
-        
+
     return JoiSchema.validate(user)
 });
 
@@ -44,15 +48,15 @@ const loginValidation = ((user) => {
 
         email: Joi.string()
             .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    
+
         password: Joi.string()
             .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
             .min(6)
             .max(100)
             .required(),
-    
+
         repeatPassword: Joi.ref('password'),
-    
+
         accessToken: [
             Joi.string(),
             Joi.number()
@@ -61,7 +65,7 @@ const loginValidation = ((user) => {
     })
         .xor('password', 'accessToken')
         .with('password', 'repeatPassword');
-        // schema options
+    // schema options
 
 
     return JoiSchema.validate(user)
