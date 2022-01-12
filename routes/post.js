b63e1d37765a045c523b5ef9d3443502b825da7a
Khailas R <khailas303@gmail.com>
2022-01-12 21:14:21 +0530

    post access
    This lets the user to login with a valid token and access their belongings.

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
+const verify = require('./tokenVerification');
 
 
-router.get('/', (req, res) => {
+router.get('/', verify, (req, res) => {
     res.json({
         posts: {
             title: 'Hello World',
             description: 'Login access only',
         }
     });
+    // res.send(req.user);  // user details. 
 });
 
 
--- routes/tokenVerification.js
+++ routes/tokenVerification.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const dotenv = require('dotenv');
 
 
 dotenv.config();
@@ -8,18 +8,18 @@ const secret_token = process.env.SECRET_TOKEN;
 const auth = ((req, res, next) => {
     const token = req.header('auth-token');
 
-    if (!token) {   // no token
+    if (!token) {   // no token. this denies the access to the post
         return res.status(401).send('Access Denied');
     }
 
-    try {
+    try {   // lets to access the post 
         const verified = jwt.verify(token, secret_token);
         req.user = verified;    // valid token
+        next();
     }
-    catch(err) {
+    catch (err) {
         res.status(400).send('Invalid Token');
     }
 });
-
 
 module.exports = auth;
