9d1c01ab1c64cff24694c56ce2dba2f9bbfb0a80
Khailas R <khailas303@gmail.com>
2022-01-12 20:39:57 +0530

    initial commit

---
+++ routes/tokenVerification.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const secret_token = process.env.SECRET_TOKEN;
+
+const auth = ((req, res, next) => {
+    const token = req.header('auth-token');
+
+    if (!token) {   // no token
+        return res.status(401).send('Access Denied');
+    }
+
+    try {
+        const verified = jwt.verify(token, secret_token);
+        req.user = verified;    // valid token
+    }
+    catch(err) {
+        res.status(400).send('Invalid Token');
+    }
+});
+
+
+module.exports = auth;
